2b04bc96b9800e8d6f0e526b718b1867aee718fc
Merely Games <rodion_kraynov@mail.ru>
2017-01-05 13:12:09 +0300

    Beginner

---
+++ merely-js.js
@@ -0,0 +1,12 @@
+/**
+
+* ----- Merely-js-engine -----
+* https://merelygames.ru
+* Copyright Merely Games 2016-2017
+* programmer: Rodion Kraynov - rodion_kraynov@mail.ru
+
+**/
+
+"use strict";
+
+
